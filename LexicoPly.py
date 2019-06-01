dd1808a25dbc580e22c8c0b21d1206469df1f192
Nezbleu <32167661+Nezbleu@users.noreply.github.com>
2019-05-31 22:49:46 -0500

    Update LexicoPly.py

--- LexicoPly.py
+++ LexicoPly.py
@@ -1,61 +1,69 @@
 import lex
-tokens = ['COMANDO_INICIAR','COMANDO_FINALIZAR','COMANDO_AVANZAR','COMANDO_GIRAR_IZQ','COMANDO_COGER_ZUMBADOR', 'COMANDO_DEJAR_ZUMBADOR', 'COMANDO_APAGAR','SENTENCIA_DEFINE_NUEVA_INSTRUCCION','SENTENCIA_COMO'
-          ,'SENTENCIA_SI','SENTENCIA_ENTONCES','SENTENCIA_SINO','SENTENCIA_REPETIR','SENTENCIA_REPETIR_VECES','SENTENCIA_MIENTRAS','SENTENCIA_MIENTRAS_HACER','OPERADOR_Y','OPERADOR_O','OPERADOR_NO','CONDICION_FRENTE_LIBRE',
-          'CONDICION_JUNTO_A_ZUMBADOR','CONDICION_ORIENTADO_AL_ESTE','CONDICION_FRENTE_BLOQUEADO','CONDICION_NO_JUNTO_A_ZUMBADOR','CONDICION_ORIENTADO_AL_OESTE','CONDICION_IZQUIERDA_LIBRE','CONDICION_ALGUN_ZUMBADOR_EN_LA_MOCHILA',
-          'CONDICION_NO_ORIENTADO_AL_NORTE','CONDICION_IZQUIERDA_BLOQUEADA','CONDICION_NINGUN_ZUMBADOR_EN_LA_MOCHILA','CONDICION_NO_ORIENTADO_AL_SUR','CONDICION_DERECHA_LIBRE','CONDICION_ORIENTADO_AL_NORTE',
-          'CONDICION_NO_ORIENTADO_AL_ESTE','CONDICION_DERECHA_BLOQUEADA','CONDICION_ORIENTADO_AL_SUR','CONDICION_NO_ORIENTADO_AL_OESTE','BOOLEANO_VERDADERO','BOOLEANO_FALSO',
-          'PUNTUACION_PUNTO_Y_COMA','PUNTUACION_LLAVES_ABRIR','PUNTUACION_LLAVES_CERRAR','CASO']#,'NAME']
+
+reserved = {
+'iniciar-programa' : 'COMANDO_INICIAR'
+, 'finalizar-programa' : 'COMANDO_FINALIZAR'
+, 'inicia-ejecucion' : 'COMANDO_INICIA'
+, 'termina-ejecucion' : 'COMANDO_TERMINA'
+, 'inicia-ejecucion' : 'COMANDO_INICIA'
+, 'terminar' : 'COMANDO_TERMINAR'
+, 'avanza' : 'COMANDO_AVANZAR'
+, 'gira-izquierda' : 'COMANDO_GIRAR_IZQ'
+, 'coge-zumbador' : 'COMANDO_COGER_ZUMBADOR'
+, 'deja-zumbador' : 'COMANDO_DEJAR_ZUMBADOR'
+, 'apagate' : 'COMANDO_APAGAR'
+, 'define-nueva-instrucion' : 'SENTENCIA_DEFINE_NUEVA_INSTRUCCION'
+, 'como' : 'SENTENCIA_COMO'
+, 'si' : 'SENTENCIA_SI'
+, 'entonces' : 'SENTENCIA_ENTONCES'
+, 'sino' : 'SENTENCIA_SINO'
+, 'repetir' : 'SENTENCIA_REPETIR'
+, 'veces' : 'SENTENCIA_REPETIR_VECES'
+, 'mientras' : 'SENTENCIA_MIENTRAS'
+, 'hacer' : 'SENTENCIA_MIENTRAS_HACER'
+, 'y' : 'OPERADOR_Y'
+, 'o' : 'OPERADOR_O'
+, 'no' : 'OPERADOR_NO'
+, 'frente-libre' : 'CONDICION_FRENTE_LIBRE'
+, 'junto-a-zumbador' : 'CONDICION_JUNTO_A_ZUMBADOR'	 
+, 'orientado-al-este' : 'CONDICION_ORIENTADO_AL_ESTE'
+, 'frente-bloqueado' : 'CONDICION_FRENTE_BLOQUEADO'	
+, 'no-junto-a-zumbador' : 'CONDICION_NO_JUNTO_A_ZUMBADOR'
+, 'orientado-al-oeste' : 'CONDICION_ORIENTADO_AL_OESTE'
+, 'izquierda-libre' : 'CONDICION_IZQUIERDA_LIBRE'
+, 'algun-zumbador-en-la-mochila' : 'CONDICION_ALGUN_ZUMBADOR_EN_LA_MOCHILA'
+, 'no-orientado-al-norte' : 'CONDICION_NO_ORIENTADO_AL_NORTE'
+, 'izquierda-bloqueada' : 'CONDICION_IZQUIERDA_BLOQUEADA'
+, 'ningun-zumbador-en-la-mochila' : 	'CONDICION_NINGUN_ZUMBADOR_EN_LA_MOCHILA'
+, 'no-orientado-al-sur' : 'CONDICION_NO_ORIENTADO_AL_SUR'
+, 'derecha-libre' : 	'CONDICION_DERECHA_LIBRE'
+, 'orientado-al-norte' : 'CONDICION_ORIENTADO_AL_NORTE'	
+, 'no-orientado-al-este' : 'CONDICION_NO_ORIENTADO_AL_ESTE'
+, 'derecha-bloqueada' : 'CONDICION_DERECHA_BLOQUEADA'	
+, 'orientado-al-sur' : 'CONDICION_ORIENTADO_AL_SUR'	
+, 'no-orientado-al-oeste' : 'CONDICION_NO_ORIENTADO_AL_OESTE'
+, 'verdadero' : 'BOOLEANO_VERDADERO'
+, 'falso' : 'BOOLEANO_FALSO'
+, 'void' : 'DECLARACION_VOID'
+, 'class' : 'DECLARACION_CLASS'
+}
+
 t_ignore = ' \n'
-t_COMANDO_INICIAR = 'iniciar-'
-t_COMANDO_FINALIZAR = 'finalizar-'
-t_COMANDO_AVANZAR = r'avanza'
-t_COMANDO_GIRAR_IZQ = 'gira-izquierda'
-t_COMANDO_COGER_ZUMBADOR = 'coge-zumbador'
-t_COMANDO_DEJAR_ZUMBADOR = 'deja-zumbador'
-t_COMANDO_APAGAR = 'apagate'
-t_SENTENCIA_DEFINE_NUEVA_INSTRUCCION = 'define-nueva-instrucion'
-t_SENTENCIA_COMO = 'como'
-t_SENTENCIA_SI = 'si'
-t_SENTENCIA_ENTONCES = 'entonces'
-t_SENTENCIA_SINO = 'sino'
-t_SENTENCIA_REPETIR = 'repetir'
-t_SENTENCIA_REPETIR_VECES = 'veces'
-t_SENTENCIA_MIENTRAS = 'mientras'
-t_SENTENCIA_MIENTRAS_HACER = 'hacer'
-t_OPERADOR_Y = 'y'
-t_OPERADOR_O = 'o'
-t_OPERADOR_NO = 'no'
-t_CONDICION_FRENTE_LIBRE = 'frente-libre'
-t_CONDICION_JUNTO_A_ZUMBADOR = 'junto-a-zumbador'	 
-t_CONDICION_ORIENTADO_AL_ESTE = 'orientado-al-este'
-t_CONDICION_FRENTE_BLOQUEADO = 'frente-bloqueado'	
-t_CONDICION_NO_JUNTO_A_ZUMBADOR = 'no-junto-a-zumbador'	
-t_CONDICION_ORIENTADO_AL_OESTE = 'orientado-al-oeste'
-t_CONDICION_IZQUIERDA_LIBRE = 'izquierda-libre'	
-t_CONDICION_ALGUN_ZUMBADOR_EN_LA_MOCHILA = 'algun-zumbador-en-la-mochila'
-t_CONDICION_NO_ORIENTADO_AL_NORTE = 'no-orientado-al-norte'
-t_CONDICION_IZQUIERDA_BLOQUEADA = 'izquierda-bloqueada'	
-t_CONDICION_NINGUN_ZUMBADOR_EN_LA_MOCHILA = 'ningun-zumbador-en-la-mochila'	
-t_CONDICION_NO_ORIENTADO_AL_SUR = 'no-orientado-al-sur'
-t_CONDICION_DERECHA_LIBRE = 'derecha-libre'	
-t_CONDICION_ORIENTADO_AL_NORTE = 'orientado-al-norte'	
-t_CONDICION_NO_ORIENTADO_AL_ESTE = 'no-orientado-al-este'
-t_CONDICION_DERECHA_BLOQUEADA = 'derecha-bloqueada'	
-t_CONDICION_ORIENTADO_AL_SUR = 'orientado-al-sur'	
-t_CONDICION_NO_ORIENTADO_AL_OESTE = 'no-orientado-al-oeste'
-t_BOOLEANO_VERDADERO = 'verdadero'
-t_BOOLEANO_FALSO = 'falso'
+t_CONECTOR = r'-' #Token temporal, no puedo hacer que lea una palabra con un conector interno --> ej: no-orientado-al-oeste
 t_PUNTUACION_PUNTO_Y_COMA = ';'
 t_PUNTUACION_LLAVES_ABRIR = '{'
 t_PUNTUACION_LLAVES_CERRAR = '}'
-t_CASO = '[r|s|t]'
-#t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
-#t_PUNTUACION_CORCHETES_ABRIR = "("
-#t_PUNTUACION_CORCHETES_CERRAR = ")"
-#t_DECLARACION_VOID = "void"
-#t_DECLARACION_CLASS = "class"
+t_PUNTUACION_CORCHETES_ABRIR = r'\('
+t_PUNTUACION_CORCHETES_CERRAR = r'\)'
 
-def t_NUMBER(t):
+tokens = ['PUNTUACION_PUNTO_Y_COMA','PUNTUACION_LLAVES_ABRIR','PUNTUACION_LLAVES_CERRAR','PUNTUACION_CORCHETES_ABRIR',
+          'PUNTUACION_CORCHETES_CERRAR', 'NOMBRE', 'NUMERO', 'CONECTOR'] + list(reserved.values())
+def t_NOMBRE(t):
+     r'[a-zA-Z_][a-zA-Z_0-9]*'
+     t.type = reserved.get(t.value,'NOMBRE') 
+     return t
+
+def t_NUMERO(t):
     r'\d+'
     t.value = int(t.value)
     return t
@@ -65,11 +73,11 @@ def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
     
-def leerArchivo(entrada="Comandos.txt"):
+def leerArchivo(entrada):
     return open(entrada, "r")
 
 lex.lex() # Build the lexer
-
+print(reserved.values())
 for i in leerArchivo(input("introduzca el nombre del archivo con la extension .txt\n")):
     lex.input(i)
     while True:
